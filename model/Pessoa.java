be3e3129cac103caddfb13bd0ae42878a3a1ff02
Joao Manoel Bandeira Coelho <jaomanel6@gmail.com>
2026-06-29 18:04:24 -0300

    Add basic domain rules and Maven setup

--- model/Pessoa.java
+++ model/Pessoa.java
@@ -4,13 +4,57 @@ import java.util.Objects;
 
 public class Pessoa {
     private final String nome;
+    private final Sexo sexo;
+    private final boolean ativo;
+    private final boolean podeSerResponsavel;
+    private final boolean podeSerAjudante;
+    private final boolean podeFazerLeitura;
+    private final boolean podeFazerDiscurso;
 
-    public Pessoa(String nome) {
+    public Pessoa(
+            String nome,
+            Sexo sexo,
+            boolean ativo,
+            boolean podeSerResponsavel,
+            boolean podeSerAjudante,
+            boolean podeFazerLeitura,
+            boolean podeFazerDiscurso
+    ) {
         this.nome = Objects.requireNonNull(nome, "nome não pode ser nulo");
+        this.sexo = Objects.requireNonNull(sexo, "sexo não pode ser nulo");
+        this.ativo = ativo;
+        this.podeSerResponsavel = podeSerResponsavel;
+        this.podeSerAjudante = podeSerAjudante;
+        this.podeFazerLeitura = podeFazerLeitura;
+        this.podeFazerDiscurso = podeFazerDiscurso;
     }
 
     public String getNome() {
         return nome;
+    }
+
+    public Sexo getSexo() {
+        return sexo;
+    }
+
+    public boolean isAtivo() {
+        return ativo;
+    }
+
+    public boolean podeSerResponsavel() {
+        return podeSerResponsavel;
+    }
+
+    public boolean podeSerAjudante() {
+        return podeSerAjudante;
+    }
+
+    public boolean podeFazerLeitura() {
+        return podeFazerLeitura;
+    }
+
+    public boolean podeFazerDiscurso() {
+        return podeFazerDiscurso;
     }
 
     @Override
--- service/GeradorEscala.java
+++ service/GeradorEscala.java
@@ -6,6 +6,7 @@ import java.util.List;
 import model.Designacao;
 import model.Parte;
 import model.Pessoa;
+import model.TipoParte;
 
 public class GeradorEscala {
     private final RegrasService regrasService;
@@ -18,18 +19,49 @@ public class GeradorEscala {
         List<Designacao> designacoes = new ArrayList<>();
         List<Pessoa> pessoasJaDesignadas = new ArrayList<>();
 
-        int indicePessoa = 0;
         for (Parte parte : partes) {
-            while (indicePessoa < pessoas.size()) {
-                Pessoa pessoa = pessoas.get(indicePessoa++);
-                if (regrasService.podeDesignar(pessoa, pessoasJaDesignadas)) {
-                    designacoes.add(new Designacao(data, parte, pessoa));
-                    pessoasJaDesignadas.add(pessoa);
-                    break;
-                }
+            if (parte.getTipo() == TipoParte.DEMONSTRACAO) {
+                designarDemonstracao(data, parte, pessoas, designacoes, pessoasJaDesignadas);
+            } else {
+                designarParteIndividual(data, parte, pessoas, designacoes, pessoasJaDesignadas);
             }
         }
 
         return designacoes;
+    }
+
+    private void designarParteIndividual(
+            LocalDate data,
+            Parte parte,
+            List<Pessoa> pessoas,
+            List<Designacao> designacoes,
+            List<Pessoa> pessoasJaDesignadas
+    ) {
+        for (Pessoa pessoa : pessoas) {
+            if (regrasService.podeDesignar(pessoa, parte, pessoasJaDesignadas)) {
+                designacoes.add(new Designacao(data, parte, pessoa));
+                pessoasJaDesignadas.add(pessoa);
+                return;
+            }
+        }
+    }
+
+    private void designarDemonstracao(
+            LocalDate data,
+            Parte parte,
+            List<Pessoa> pessoas,
+            List<Designacao> designacoes,
+            List<Pessoa> pessoasJaDesignadas
+    ) {
+        for (Pessoa responsavel : pessoas) {
+            for (Pessoa ajudante : pessoas) {
+                if (regrasService.podeFormarDemonstracao(responsavel, ajudante, pessoasJaDesignadas)) {
+                    designacoes.add(new Designacao(data, parte, responsavel, ajudante));
+                    pessoasJaDesignadas.add(responsavel);
+                    pessoasJaDesignadas.add(ajudante);
+                    return;
+                }
+            }
+        }
     }
 }
