4b46649aac34e192203cbcb738c99d24a1c7f068
Joao Manoel Bandeira Coelho <jaomanel6@gmail.com>
2026-07-24 10:14:00 -0300

    Criar MainApp.java

---
+++ src/main/java/br/com/geradordesignacoes/MainApp.java
@@ -0,0 +1,28 @@
+package br.com.geradordesignacoes;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+
+    @Override
+    public void start(Stage stage) {
+
+        Label label = new Label("Gerador de Designações v0.8");
+
+        StackPane root = new StackPane(label);
+
+        Scene scene = new Scene(root, 800, 600);
+
+        stage.setTitle("Gerador de Designações");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
